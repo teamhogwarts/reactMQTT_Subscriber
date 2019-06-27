d2c6dc4d4026dd80be2b12a8a2adc5728a7a4519
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-06-27 15:40:25 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import {SubscribeForm} from "./components/SubscribeForm";
 import {MessageItem} from "./components/MessageItem";
+import Mqtt from 'mqtt';
 
+let client;
 
 export default class App extends React.Component {
 
     state = {
         topics: [],
-        message: [
+        messages: [
             {
                 topic: "News",
                 text: "Whatzuuup!"
@@ -21,17 +23,33 @@ export default class App extends React.Component {
     }
 
     componentDidMount() {
+        const SERVER_URL = process.env.REACT_APP_MQTT_SERVER_URL;
+        const USERNAME = process.env.REACT_APP_USERNAME;
+        const PASSWORD = process.env.REACT_APP_PASSWORD;
 
+        console.log("using server '%s'", SERVER_URL);
 
+        client = Mqtt.connect('mqtt://' + SERVER_URL, {
+            username: USERNAME,
+            password: PASSWORD,
+            clientId: 'mqttclient_' + Math.random()
+        });
+
+        client.on('connect', function () {
+            console.log('connected');
+            // client.subscribe('voting');
+        });
+
+        client.on('message', this.messageHandler);
     }
 
 
     render() {
         return (
             <div>
-                <h1>Eingabe</h1>
-                <SubscribeForm/>
-                {this.state.message.map((messageItem, i) => <MessageItem key={i}
+                <h1>WELCOME TO SUBSCRIBER CLIENT</h1>
+                <SubscribeForm subscribeHandler={(topic) => this.subscribe(topic)}/>
+                {this.state.messages.map((messageItem, i) => <MessageItem key={i}
                                                                          messageItem={messageItem}
                     />
                 )}
@@ -39,5 +57,23 @@ export default class App extends React.Component {
         )
     }
 
+    messageHandler = (topic, message) => {
+        console.log("message received over topic '%s'", topic);
 
+        const response = JSON.parse(message);
+
+        this.setState( state => ({
+            topics: [...state.topics, response.topic],
+            messages: [...state.messages, response.text]
+        }));
+    };
+
+    subscribe = (topic) => {
+        alert(topic);
+        //client.subscribe(topic);
+    };
+
+    componentWillUnmount() {
+        // client.end(true);
+    }
 }
--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Col, Input, Row, Card, CardBody, CardTitle, Button} from "reactstrap";
+import {Col, Input, Row, Card, CardBody, CardTitle} from "reactstrap";
 
 
-export const SubscribeForm = () =>
+export const SubscribeForm = (subscribeHandler) =>
     <Row>
         <Col md="6">
             <Card body inverse color="dark" className="mx-5">
                 <CardBody>
                     <CardTitle><strong>Subribe to Topic</strong></CardTitle>
-                    <Input type="text"
-                           placeholder={'Enter the Topic'}
-                    />
-
-                    <Button color="primary">Subscribe</Button>
+                    <form onSubmit={(topic) => subscribeHandler(topic)}>
+                        <Input type="text"
+                               placeholder={'Enter the Topic'}
+                               name="topic"
+                        />
+                        <Input color="primary" type="submit" value="Subscribe"/>
+                    </form>
                 </CardBody>
             </Card>
         </Col>
